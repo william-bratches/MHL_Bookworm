e0e3a497237d3a6e77bf348325084473944cceba
william-bratches <william.bratches@gmail.com>
2014-06-14 17:16:32 -0400

    added simple directory loop

--- parsers/parseOL.py
+++ parsers/parseOL.py
@@ -4,56 +4,68 @@
 
 from xml.dom.minidom import parseString
 import re
+import os, sys
 import subprocess
-from lxml import etree
+#from lxml import etree
 #may not need some of these, preserve memory
 
 
+#folder navigation
+def exploreDir():
+    # path = '/Desktop/MHL_Bookworm/samples/'
+    dirs = os.listdir('.')
+    folders = []
+    for folder in dirs:
+        print folder  #debugging
+        folders.append(folder)
+exploreDir()
+"""
 globalId = "abdominalsurgery00smit"
-#TO DO: have the parser automatically swap IDs from folder
 
 def readInput():
-	file = open(globalId + "_djvu.txt") #archive.org extension
-	data = file.read()
-	file.close()
-	#this can probably be abstracted into one function for both files
-	#or maybe separated into two different parsing scripts
+    file = open(globalId + "_djvu.txt") #archive.org extension
+    data = file.read()
+    file.close()
+    #this can probably be abstracted into one function for both files
+    #or maybe separated into two different parsing scripts
 
 
-	def buildInput():
-		text = re.sub("[\n\r]","",data)
-		subprocess.call(['touch', 'input.txt'])                       #will probably need to break into two functions
-		inp = open('input.txt', 'w')                                      #so input.txt is not opened with every single file (improve speed)
-		inp.write(globalId + "    " + text + '/n')
-		inp.close()
-	buildInput()
+    def buildInput():
+        text = re.sub("[\n\r]","",data)
+        subprocess.call(['touch', 'input.txt'])                       #will probably need to break into two functions
+        inp = open('input.txt', 'w')                                      #so input.txt is not opened with every single file (improve speed)
+        inp.write(globalId + "    " + text + '/n')
+        inp.close()
+    buildInput()
 readInput()
-
+"""
 
 
 
 #build metadata entries
-
+"""
 def readJSON():
-	jsonfile = open(globalId + '_dc.xml')
-	jdata = jsonfile.read()
-	file.close()
-	dom = parseString(data)
-	metaData = dom.getElementsByTagName("div")
+    jsonfile = open(globalId + '_dc.xml')
+    jdata = jsonfile.read()
+    file.close()
+    dom = parseString(data)
+    metaData = dom.getElementsByTagName("div")
 
 
-	def parseMetadata(info, dataNum):
-    		root = etree.fromstring(jdata)
-	 	 subprocess.call(['touch', 'jsoncatalog.txt'])
-	 	 jtxt = open('jsoncatalog.txt', 'w')
-	 	 	#tags to search go here
-	 	 	for element in root.iter("date")
-	 	 		jtxt.write("{ date: ") + '"' element.items()) #tag needs to be stripped
-				+ '", "filename:" ' + globalId + "searchstring:" + url + "}"    #better way to do this?
-	 	 	for element in root.iter("contributor")
+    def parseMetadata(info, dataNum):
+            root = etree.fromstring(jdata)
+         subprocess.call(['touch', 'jsoncatalog.txt'])
+         jtxt = open('jsoncatalog.txt', 'w')
+            #tags to search go here
+            for element in root.iter("date")
+                jtxt.write("{ date: ") + '"' element.items()) #tag needs to be stripped
+                + '", "filename:" ' + globalId + "searchstring:" + url + "}"    #better way to do this?
+            for element in root.iter("contributor")
                                             #same code for contributor tag tag
                        jtxt.close()
 
-	
-	 	 parseMetadata()
+    
+         parseMetadata()
 readJSON()
+"""
+
